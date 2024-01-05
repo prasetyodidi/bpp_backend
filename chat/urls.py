2dc14ac44ee90404a023c5f4eac1fc309ecc8eac
Didi Prasetyo <didiprasetyo36@gmail.com>
2024-01-05 14:49:28 +0700

    chat route

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,11 @@ from chat import views
 
 app_name = 'chat'
 urlpatterns = [
-    path('create-user-and-group', views.create_user_and_group, name='create_user_and_group')
+    path('create-user-and-group', views.create_user_and_group, name='create_user_and_group'),
+    path('create-user-and-join-group', views.create_user_and_join_group, name='create_user_and_join_group'),
+    path('groups', views.user_groups, name='user_groups'),
+    path('groups/join', views.join_group, name='user_join_group'),
+    path('messages', views.add_message, name='user_add_message'),
+    path('groups/<int:group_id>/change-time-period', views.change_time_period, name='user_change_group_time_period'),
+    path('groups/<int:group_id>', views.messages_by_group_id, name='get_messages_by_group_id'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,38 +1,199 @@
 import time
 
 from django.contrib.auth.models import User
-from chat.models import Group, ActivePeriod
+from rest_framework import serializers
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from chat.models import Group, ActivePeriod, Message
 from rest_framework.response import Response
 from django.utils.crypto import get_random_string
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from django.shortcuts import get_object_or_404
 
+from chat.mqtt import client as mqtt_client
+import json
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
+        
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
 
 @api_view(['POST'])
 def create_user_and_group(request):
     username = request.data['username']
-    group_name = request.data['group_name']
+    group_name = request.data['groupName']
     password = get_random_string(length=6)
 
-    user = User.objects.create_user(username=username)
-    user.set_password(password)
+    user = User.objects.create_user(username=username, password=password)
     user.save()
 
     active_period = ActivePeriod.objects.first()
-    # group = Group.objects.create(name=group_name, owner_id=user, active_period=active_period, created_at=time.time())
-    group = Group.objects.create(name=group_name, active_period=active_period, created_at=time.time())
+    group_code = get_random_string(6)
+    group = Group.objects.create(
+        name=group_name,
+        owner=user,
+        active_period=active_period,
+        created_at=int(time.time()),
+        code=group_code
+    )
+
     group.save()
 
     group.members.add(user)
 
-    return Response({'account': {'username': username, 'password': password}, 'group': {'name': group.name}})
+    return Response({
+        "account": {
+            "username": username,
+            "password": password
+        },
+        "group": {
+            "name": group_name,
+            "code": group_code
+        }
+    })
+    
+
+@api_view(['POST'])
+def create_user_and_join_group(request):
+    username = request.data['username']
+    group_code = request.data['groupCode']
+    password = get_random_string(length=6)
+
+    user = User.objects.create_user(username=username, password=password)
+    user.save()
+    
+    group = get_object_or_404(Group.objects.filter(code=group_code))
+    group.members.add(user)
+    
+    web_response = {
+        'account': {
+            'name': user.username,
+            'password': password,
+        },
+        'group': {
+            'id': group.id,
+            'name': group.name,
+        }
+    }
+
+    return Response(web_response)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def user_groups(request):
+    user = request.user
+
+    data = GroupSerializer(Group.objects.filter(members__id=user.id), many=True).data
+    return Response(data)
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def messages_by_group_id(request, group_id):
+    group = get_object_or_404(Group.objects.filter(id=group_id))  
+    
+    messages = Message.objects.prefetch_related('owner').filter(group_id=group_id)
+    
+    data_messages = []
+    for message in messages:
+        data_messages.append({
+            "id": message.id,
+            "message": message.message,
+            "group": 21,
+            'owner': {
+                'username': message.owner.username,
+            }
+        })
+
+    data = GroupSerializer(group, many=False).data
+    messages_data = MessageSerializer(messages, many=True).data
+    return Response(data_messages)
+
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def join_group(request):
+    group_code = request.data['group_code']
+
+    user = request.user
+
+    group = get_object_or_404(Group.objects.filter(code=group_code))
+    group.members.add(user)
+
+    groups_data = GroupSerializer(Group.objects.filter(members__id=user.id), many=True).data
+    return Response({'groups': groups_data})
 
 
 @api_view(['POST'])
-def create_chat(request):
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def add_message(request):
     text = request.data['text']
     group_id = request.data['groupId']
+
+    user = request.user
+    group = get_object_or_404(Group.objects.filter(id=group_id))
+
+    message = Message.objects.create(message=text, owner=user, group=group)
+    message.save()
     
-    group = get_object_or_404(Group, id=group_id)
+    mqtt_message = {
+        "key": message.id,
+        "msg": text,
+        "username": user.username
+    }
     
-    return Response({'message': 'Hello World!'})
+    json_message = json.dumps(mqtt_message)
+    
+    topic = "group/" + group.code
+    
+    msg_count = 1
+    
+    while True:
+        time.sleep(1)
+        result = mqtt_client.publish(topic, json_message)
+        status = result[0]
+        if status == 0:
+            print(f"Send `{json_message}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+        if msg_count > 5:
+            break
+
+    return Response({'message': text, 'topic': topic, 'result': result[0]})
+
+
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def change_time_period(request, group_id):
+    active_period_id = request.data['activePeriodId']
+    active_period = ActivePeriod.objects.filter(id=active_period_id).first()
+    
+    group = Group.objects.filter(id=group_id).first()
+    group.active_period = active_period
+    group.save()
+    
+    group_data = {
+        'name': group.name,
+        'activePeriodId': group.active_period.id
+    }
+    
+    return Response({'message': 'changed ', 'groupId': group_id, 'group': group_data, 'activePeriodId': active_period_id})
