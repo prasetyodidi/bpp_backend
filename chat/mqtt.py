ae851a32d73cc5486f08dd482d5bbaa453f2f0db
Didi Prasetyo <didiprasetyo36@gmail.com>
2024-01-08 04:36:57 +0700

    on_disconnect handle

--- chat/mqtt.py
+++ chat/mqtt.py
@@ -1,3 +1,5 @@
+import time
+
 import paho.mqtt.client as mqtt
 from django.conf import settings
 
@@ -12,9 +14,29 @@ def on_connect(mqtt_client, userdata, flags, rc):
 
 def on_message(mqtt_client, userdata, msg):
     print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
+    
+def on_disconnect(client, userdata, rc):
+    print.info("Disconnected with result code: %s", rc)
+    reconnect_count, reconnect_delay = 0, settings.MQTT_FIRST_RECONNECT_DELAY
+    while reconnect_count < settings.MQTT_MAX_RECONNECT_COUNT:
+        print.info("Reconnecting in %d seconds...", reconnect_delay)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print.info("Reconnected successfully!")
+            return
+        except Exception as err:
+            print.error("%s. Reconnect failed. Retrying...", err)
+
+        reconnect_delay *= settings.MQTT_RECONNECT_RATE
+        reconnect_delay = min(reconnect_delay, settings.MQTT_MAX_RECONNECT_DELAY)
+        reconnect_count += 1
+    print.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)    
 
 client = mqtt.Client()
 client.on_connect = on_connect
+client.on_disconnect = on_disconnect
 client.on_message = on_message
 client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
 client.connect(
